dba6a9b9e5a364a6d5ab5414804c21f990f2c7a3
Harendra Kumar <harendra.kumar@gmail.com>
2017-09-12 16:22:22 +0530

    Simplify the example

--- Main.hs
+++ Main.hs
@@ -4,12 +4,19 @@ module Main where
 
 import Async
 import           Criterion.Main (defaultMain, bench, nfIO)
-import           Data.Semigroup              (Semigroup(..))
+import Data.Monoid
 
 main :: IO ()
 main = defaultMain [ bench "asyncly-serial" $ nfIO asyncly_basic]
 
 asyncly_basic :: IO Int
 asyncly_basic = do
-    xs <- toList $ (foldr (<>) mempty $ Prelude.map return [1..100000 :: Int])
-    return (Prelude.length xs)
+    xs <- toList $ (foldr (<>) mempty $ map (\x -> Yield x Stop) [1..100000 :: Int])
+    return (length xs)
+
+{-
+    where
+    {-# NOINLINE xfoldr #-}
+    xfoldr = foldr
+    xmap = map
+    -}
--- src/Async.hs
+++ src/Async.hs
@@ -2,59 +2,20 @@
 
 module Async where
 
-import           Data.Semigroup              (Semigroup(..))
-import           Control.Monad               (ap, liftM)
+import           Control.Monad               (liftM)
 
-newtype AsyncT m a =
-    AsyncT {
-        runAsyncT :: forall r.
-               Maybe Int                          -- state
-            -> m r                                          -- stop
-            -> (a -> Maybe Int -> Maybe (AsyncT m a) -> m r)  -- yield
-            -> m r
-    }
+data AsyncT a = Stop | Yield a (AsyncT a)
 
-------------------------------------------------------------------------------
--- Monad
-------------------------------------------------------------------------------
+instance Monoid (AsyncT a) where
+    mempty = Stop
+    mappend x y =
+        case x of
+            Stop -> y
+            Yield a r -> Yield a (mappend r y)
 
--- | Appends the results of two AsyncT computations in order.
-instance Semigroup (AsyncT m a) where
-    (AsyncT m1) <> m2 = AsyncT $ \ctx stp yld ->
-        let stop = (runAsyncT m2) ctx stp yld
-            yield a c Nothing  = yld a c (Just m2)
-            yield a c (Just r) = yld a c (Just (mappend r m2))
-        in m1 ctx stop yield
-
--- | Appends the results of two AsyncT computations in order.
-instance Monoid (AsyncT m a) where
-    mempty = AsyncT $ \_ stp _ -> stp
-    mappend = (<>)
-
-instance Monad m => Monad (AsyncT m) where
-    return a = AsyncT $ \ctx _ yld -> yld a ctx Nothing
-
-    AsyncT m >>= f = AsyncT $ \_ stp yld ->
-        let run x = (runAsyncT x) Nothing stp yld
-            yield a _ Nothing  = run $ f a
-            yield a _ (Just r) = run $ f a <> (r >>= f)
-        in m Nothing stp yield
-
-instance Monad m => Functor (AsyncT m) where
-    fmap = liftM
-
-instance Monad m => Applicative (AsyncT m) where
-    pure  = return
-    (<*>) = ap
-
-toList :: Monad m => AsyncT m a -> m [a]
-toList m = run Nothing m
-
-    where
-
-    stop = return []
-
-    yield a _ Nothing  = return [a]
-    yield a c (Just x) = liftM (a :) (run c x)
-
-    run ctx mx = (runAsyncT mx) ctx stop yield
+-- {-# INLINE toList #-}
+toList :: Monad m => AsyncT a -> m [a]
+toList m =
+    case m of
+        Stop -> return []
+        Yield a r -> liftM (a :) (toList r)
