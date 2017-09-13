da79baf47cba11b9e3cd84604a464ada06aee4a0
Harendra Kumar <harendra.kumar@gmail.com>
2017-09-13 08:49:04 +0530

    Add single module case under CPP macros

--- Main.hs
+++ Main.hs
@@ -1,13 +1,40 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
-import List
 import Criterion.Main (defaultMain, bench, nfIO)
-import Data.Monoid
 
-main :: IO ()
-main = defaultMain [ bench "len" $ nfIO len]
+-- Uncomment this to have all the code in one module
+-- #define SINGLE_MODULE
+#ifndef SINGLE_MODULE
+import List
+#else
+import Control.Monad  (liftM)
+
+data List a = Stop | Yield a (List a)
+
+instance Monoid (List a) where
+    -- {-# INLINE mempty #-}
+    mempty = Stop
+    -- {-# INLINE mappend #-}
+    mappend x y =
+        case x of
+            Stop -> y
+            Yield a r -> Yield a (mappend r y)
+
+-- {-# NOINLINE toList #-}
+toList :: Monad m => List a -> m [a]
+toList m =
+    case m of
+        Stop -> return []
+        Yield a r -> liftM (a :) (toList r)
+
+#endif
 
 len :: IO Int
 len = do
-    xs <- toList $ (foldr (<>) mempty $ map (\x -> Yield x Stop) [1..100000 :: Int])
+    xs <- toList $ (foldr mappend mempty $ map (\x -> Yield x Stop) [1..100000 :: Int])
     return (length xs)
+
+main :: IO ()
+main = defaultMain [ bench "len" $ nfIO len]
